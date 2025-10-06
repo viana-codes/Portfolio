6cfc701d0a7eb7e40f41b93c137c462cf10355a9
Viana Kalversberg <vkalversberg@gmail.com>
2025-10-06 12:17:32 +0200

    refactor Hero component; replace UColorModeAvatar with UColorModeImage, update button label, and remove unused UMarquee

--- app/components/landing/Hero.vue
+++ app/components/landing/Hero.vue
@@ -33,11 +33,13 @@ defineProps<{
           delay: 0.1
         }"
       >
-        <UColorModeAvatar
-          class="size-18 ring ring-default ring-offset-3 ring-offset-(--ui-bg)"
+        <UColorModeImage
           :light="global.picture?.light!"
           :dark="global.picture?.dark!"
           :alt="global.picture?.alt!"
+          width="144"
+          height="144"
+          class="w-18 h-18 rounded-full ring ring-default ring-offset-3 ring-offset-(--ui-bg)"
         />
       </Motion>
     </template>
@@ -105,13 +107,12 @@ defineProps<{
           v-if="page.hero.links"
           class="flex items-center gap-2"
         >
-          <UButton v-bind="page.hero.links[0]" />
           <UButton
             :color="global.available ? 'success' : 'error'"
             variant="ghost"
             class="gap-2"
             :to="global.available ? global.meetingLink : ''"
-            :label="global.available ? 'Available for new projects' : 'Not available at the moment'"
+            :label="global.available ? 'Available for new opportunities' : 'Not available at the moment'"
           >
             <template #leading>
               <span class="relative flex size-2">
@@ -156,36 +157,6 @@ defineProps<{
       </div>
     </template>
 
-    <UMarquee
-      pause-on-hover
-      class="py-2 -mx-8 sm:-mx-12 lg:-mx-16 [--duration:40s]"
-    >
-      <Motion
-        v-for="(img, index) in page.hero.images"
-        :key="index"
-        :initial="{
-          scale: 1.1,
-          opacity: 0,
-          filter: 'blur(20px)'
-        }"
-        :animate="{
-          scale: 1,
-          opacity: 1,
-          filter: 'blur(0px)'
-        }"
-        :transition="{
-          duration: 0.6,
-          delay: index * 0.1
-        }"
-      >
-        <NuxtImg
-          width="234"
-          height="234"
-          class="rounded-lg aspect-square object-cover"
-          :class="index % 2 === 0 ? '-rotate-2' : 'rotate-2'"
-          v-bind="img"
-        />
-      </Motion>
-    </UMarquee>
+   
   </UPageHero>
 </template>
