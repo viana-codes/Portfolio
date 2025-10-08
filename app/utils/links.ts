f293b356244cd5408108b704e5ba24f1602adc79
Viana Kalversberg <vkalversberg@gmail.com>
2025-10-08 17:15:26 +0200

    disable about for now

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -12,8 +12,8 @@ export const navLinks: NavigationMenuItem[] = [{
   label: 'Blog',
   icon: 'i-lucide-file-text',
   to: '/blog'
-},*/ {
+}, {
   label: 'About',
   icon: 'i-lucide-user',
   to: '/about'
-}]
+}*/]
